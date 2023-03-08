ce4368c3636b40ecfc427a648f5f8b2061cf9c2d
iArti <artis2000@mail.ru>
2023-03-09 01:03:03 +0300

    Update clone.sh
    Try to make it elish

--- clone.sh
+++ clone.sh
@@ -1,25 +1,13 @@
-rm -rf device/xiaomi/enuma
+rm -rf device/xiaomi/elish
 rm -rf vendor/xiaomi
 rm -rf kernel/xiaomi
 cd ./device/
 mkdir xiaomi
 cd ./xiaomi/
-git clone https://github.com/guosasipo/android_device_xiaomi_enuma.git
-mv android_device_xiaomi_enuma enuma
-cd ../../
-cd ./vendor/
-mkdir xiaomi
-cd ./xiaomi/
-git clone https://github.com/guosasipo/android_vendor_xiaomi_enuma.git
-mv android_vendor_xiaomi_enuma enuma
-cd ../../
-cd ./kernel/
-mkdir xiaomi
-cd ./xiaomi/
-git clone https://github.com/UtsavBalar1231/kernel_xiaomi_sm8250.git
-mv kernel_xiaomi_sm8250 enuma
+git clone https://github.com/iArti/android_device_xiaomi_elish.git
+mv android_device_xiaomi_elish elish
 cd ../../
 . build/envsetup.sh
-lunch arrow_enuma-userdebug
+lunch arrow_elish-userdebug
 m clean
 m bacon
